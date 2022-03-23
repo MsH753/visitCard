6be33187eeded69f2c3047d921e2cbe520cc9b8d
John Doe <MrakobesPlay@mail.ru>
2022-03-23 21:20:38 +0700

    IhateVers

--- card/src/component/Header.js
+++ card/src/component/Header.js
@@ -11,26 +11,44 @@ const Title = styled.div`
 `;
 const Wrapper = styled.div`
   display: flex;
-  align-items: center;
+  align-content: center;
+  
   justify-content: space-between;
   padding: 32px 80px;
   
 `;
-const meImage = styled.div`
+const MeImage = styled.div`
     display: flex;
+    
+    
+    height: 52px;
+    
+    
+    
+    
 `
 const NameImg = styled.div`
-    border: 1px solid black;
+    display: flex;
+    padding-left: 20px;
+    font-family: 'Gilroy';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 18px;
+    
+    align-items: center;
+    
 `;
 
 
 const Header = () => {
   return (
     <Wrapper>
-        <meImage>
-            <NameImg><img  src={meHeader}/></NameImg>
+        <MeImage>
+            <img  src={meHeader}/>
+            <NameImg>Алейников Ярослав</NameImg>
             
-        </meImage>
+        </MeImage>
     
     
    </Wrapper>
